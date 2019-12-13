4de7cbc87739c012a171df14c8868982d9d9f29d
Michael Crumm <mike@crumm.net>
2019-12-13 09:56:47 -0800

    add basic on-demand producer

---
+++ lib/off_broadway/imps/on_demand_producer.ex
@@ -0,0 +1,47 @@
+defmodule OffBroadway.Imps.OnDemandProducer do
+  @moduledoc """
+  A `Broadway` producer that replies when dequeuing messages.
+
+  When called from the `c:Broadway.handle_batch/4` callback
+  of a pipeline, this producer will reply when the first
+  message in the batch has been dispatched to a consumer,
+  allowing effective acknowledgement of the messages to
+  continue in the calling pipeline.
+
+  Messages will be enqueued in this pipeline until there is
+  consumer demand.
+  """
+  use GenStage
+  import OffBroadway.Imps.QueueHelpers
+
+  @impl GenStage
+  def init(_opts) do
+    {:producer,
+     %{
+       queue: :queue.new(),
+       demand: 0
+     }}
+  end
+
+  @impl GenStage
+  def handle_call({InMemoryProducer, :enqueue, messages}, from, state) do
+    %{demand: demand, queue: queue} = state
+
+    # TODO: limit the queue size
+    {demand, messages, queue} =
+      queue
+      |> enqueue_many(from, messages)
+      |> dequeue_many(demand)
+
+    {:noreply, messages, %{state | demand: demand, queue: queue}}
+  end
+
+  @impl GenStage
+  def handle_demand(incoming_demand, state) do
+    %{demand: demand, queue: queue} = state
+
+    {demand, messages, queue} = dequeue_many(queue, demand + incoming_demand)
+
+    {:noreply, messages, %{state | queue: queue, demand: demand}}
+  end
+end
