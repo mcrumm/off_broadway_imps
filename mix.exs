fe3effd4bba1dfe6e647fe7fe047950a86793d6e
Michael Crumm <mike@crumm.net>
2019-12-18 20:45:44 -0800

    Match Broadway requirements

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OffBroadway.Imps.MixProject do
     [
       app: :off_broadway_imps,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
